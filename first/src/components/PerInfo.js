fa273f2ce3bfdc75f3eec962f7f60f94ef966439
paudel54 <2sansrit@gmail.com>
2023-06-05 14:32:46 +0545

    props are immutable and concept of state in react came to handle dynamics

--- first/src/components/PerInfo.js
+++ first/src/components/PerInfo.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 const PerInfo = (props) => {
     // Object Destructuring
+    // Props are immutable and concept of state came to make it more dynamic. 
+    // props are attribute passed from components in form of object literals and are later destructured to access 
     const { name, age } = props;
     return (
         <div>
