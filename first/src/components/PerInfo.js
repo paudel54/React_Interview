b7d0f2f0089edef0b74e892df325033239d6bad1
paudel54 <2sansrit@gmail.com>
2023-06-05 14:29:34 +0545

    revisited object literals in passing props

--- first/src/components/PerInfo.js
+++ first/src/components/PerInfo.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
 const PerInfo = (props) => {
+    // Object Destructuring
+    const { name, age } = props;
     return (
         <div>
 
             <div class="test">
-                {/* {JSON.stringify(props)} */}
-                <h2> Name : {props.name}</h2>
-                <h2> Age :{props.age}</h2>
+                {JSON.stringify(props)}
+                <h2> Name : {name}</h2>
+                <h2> Age :{age}</h2>
             </div>
 
         </div>
