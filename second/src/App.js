eacedb442c1f733eb64d9c0864c146951b95e117
paudel54 <2sansrit@gmail.com>
2023-06-07 05:44:46 +0545

    Clearified expression and String Literals

--- second/src/App.js
+++ second/src/App.js
@@ -1,32 +1,11 @@
-import './App.css';
-import { useState } from 'react';
-import ListItem from './component/ListItem';
-// Rendering the collection of list in React with map() fn. 
-function App() {
-  // array of object literals
-  const temp = [
-    { id: 1, pName: 'Apple', price: '$12' },
-    { id: 2, pName: 'Orange', price: '$9' },
-    { id: 3, pName: 'Lemon', price: '$2' },
-    { id: 4, pName: 'Kiwi', price: '$7' },
-  ]
-  const [products, setProducts] = useState(temp);
-  // Creating Mapping function seperate to make the code much more clearner. 
-  let productList = (
-    products.map((item) => {
-      return (
-        <ListItem key={item.id.toString()} pName={item.pName} />
-      )
-    })
-  )
+import React from 'react'
+import DemoComponent from './component/DemoComponent'
+const App = () => {
   return (
-    <div className="App">
-      <div>THis is List and PROPS</div>
-      {/* {JSON.stringify(products)} */}
-      <div>
-        {productList}
-      </div>
+    <div>
+      <DemoComponent value={1 + 4} date={new Date()} lekh='String literals not required curly brackets' />
     </div>
-  );
+  )
 }
-export default App;
+
+export default App
---
+++ second/src/component/DemoComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DemoComponent = (props) => {
+    return (
+        <div>
+            <p>Demo Component</p>
+            {JSON.stringify(props)}
+            <hr />
+            <h2>Value :- {props.value}</h2>
+            <h2>Date :- {props.date.toString()}</h2>
+            <h3>String literals :- {props.lekh}</h3>
+        </div>
+    )
+}
+
+export default DemoComponent
