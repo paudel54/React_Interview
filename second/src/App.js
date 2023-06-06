e2534a1592b163d4a74e12dcc777aeb583243546
paudel54 <2sansrit@gmail.com>
2023-06-07 04:36:38 +0545

    map() method to render the collection of list

--- second/src/App.js
+++ second/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-
+import { useCallback, useState } from 'react';
+// Rendering the collection of list in React with map() fn. 
 function App() {
+  const temp = ['a', 'b', 'c', 'd']
+  const [product, setProducts] = useState(temp);
   return (
     <div className="App">
       <div>THis is List and PROPS</div>
+      {product.map((item) => {
+        return (
+          <ul>
+            <li>{item}</li>
+          </ul>
+        )
+      })}
     </div>
   );
 }
