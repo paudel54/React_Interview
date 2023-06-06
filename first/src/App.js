8368d8cd31cf1e778e427e33debb9382cd4fa521
paudel54 <2sansrit@gmail.com>
2023-06-06 09:11:29 +0545

    Clearified the use effect for multipe State controls

--- first/src/App.js
+++ first/src/App.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
 
-export default function App() {
-  const [cTime, setTime] = useState(new Date().toLocaleString());
 
+let count = 0;
+export default function App() {
+  const [cnt, setCount] = useState(0);
+  const [flag, setFlag] = useState(true);
+  // Use effect is called everytime there is a re-render
   useEffect(() => {
-    setInterval(() => {
-      setTime(new Date().toLocaleString())
-    }, 1000);
-  })
+    console.log('Effects called ... ' + count++);
+  }, [cnt])
 
   return (
     <div>
-      <h1>{cTime}</h1>
-      <button onClick={() => setTime(new Date().toLocaleString())}>Increment</button>
+      <h1>{cnt + '-' + flag.toString()}</h1>
+      <button onClick={() => setCount(cnt + 1)}>Increment</button>
+      <button onClick={() => setFlag(!flag)}>Toggle</button>
     </div>
   );
 }
---
+++ first/src/components/Code_reserve/digialClock_useEffect.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+    const [cTime, setTime] = useState(new Date().toLocaleString());
+
+    useEffect(() => {
+        setInterval(() => {
+            setTime(new Date().toLocaleString())
+        }, 1000);
+    })
+
+    return (
+        <div>
+            <h1>{cTime}</h1>
+            <button onClick={() => setTime(new Date().toLocaleString())}>Increment</button>
+        </div>
+    );
+}
+
