951333c3938b1db4690e91686c991d02f0e4646b
paudel54 <2sansrit@gmail.com>
2023-06-06 08:13:00 +0545

    Clearfied the concept of class component lifeCycle and react and its replacement with functional useEffect() hook

--- first/src/components/class.js
+++ first/src/components/class.js
@@ -9,12 +9,39 @@ export default class functional extends Component {
     state = {
         counter: 0
     }
+
+    increment = () => {
+        let c = this.state.counter;
+        c++;
+        this.setState((state, props) => {
+            return {
+                counter: c
+            }
+        }, () => { console.log('The Component is rendered') })
+    }
+
     render() {
         return (
             <div>
                 I am functional Component
                 <h1>{this.state.counter}</h1>
+                <button onClick={this.increment}> Increment</button>
             </div>
         )
     }
 }
+
+// class component has setState() predefined keyword.  its request to update for state makes queue for updating component.
+// setState( changeObject, [optional callback])
+
+
+// Optional Callback function is called once the  setState() completes the execution and the component is re-rendered.
+
+
+// What is the component life cycle method in class component?
+// When you have a class component, there are various phases where the component life cycle methods are getting executed automatically
+// like constructor() render(), componentWillMount(), shouldComponentUpdate() , when you have to write some code at a particualr sequence then you will use the
+// component life cycle methods
+
+// phases mounting, updating, unmounting, error handling,
+//purpose execute peice of code depending on the peice of sequence, or requirements. 
