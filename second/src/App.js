502165c0ffab7f489194811e30f003ea44b2ccb9
paudel54 <2sansrit@gmail.com>
2023-06-07 05:08:21 +0545

    Revisited List with Components

--- second/src/App.js
+++ second/src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
+import ListItem from './component/ListItem';
 // Rendering the collection of list in React with map() fn. 
 function App() {
-  const temp = ['a', 'b', 'c', 'd']
-  const [product, setProducts] = useState(temp);
+  // array of object literals
+  const temp = [
+    { id: 1, pName: 'Apple', price: '$12' },
+    { id: 2, pName: 'Orange', price: '$9' },
+    { id: 3, pName: 'Lemon', price: '$2' },
+    { id: 4, pName: 'Kiwi', price: '$7' },
+  ]
+  const [products, setProducts] = useState(temp);
   return (
     <div className="App">
       <div>THis is List and PROPS</div>
+      {/* {JSON.stringify(products)} */}
       <div>
-        {product.map((item, i) => {
+        {products.map((item) => {
           return (
-            <li key={i.toString()}>{item}</li>
+            <ListItem key={item.id.toString()} pName={item.pName} />
           )
         })}
       </div>
---
+++ second/src/component/ListItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ListItem = (props) => {
+    return (
+        <div>
+            <ul>
+                <li>{props.pName}</li>
+            </ul>
+        </div>
+    )
+}
+
+export default ListItem
