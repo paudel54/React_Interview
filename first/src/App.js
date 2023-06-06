15f35d1b525a434ce99bcadb8016c3c6908d8ff1
paudel54 <2sansrit@gmail.com>
2023-06-06 05:55:09 +0545

    Clearified basic event Handling and Parameter Passing, with application of arrow fn in event handling

--- first/src/App.js
+++ first/src/App.js
@@ -5,16 +5,39 @@ import React, { useState } from 'react'
 
 function App() {
   // destructuring the fn setter function setCount form useState hook , adding 0 as defalut value.  
-  const [i, setCount] = useState(0);
-  // let i = 0;
-  const addValue = () => {
-    setCount(i + 1);
-    console.log(i);
+  // How do you create a state in a functional component or what is the purpose of useState() methods?
+  // State is like a variable which is watched by React Application if therer are any change into state React will make sure that they are Rerendered. 
+
+
+  // lazy initialization if we want to load non primitive data on state
+  function initialState() {
+    // lazy initialization is required if you have a bigger porcess which is going to give you the initial state value and that you put in a function. then instead of writing
+    //the function only do make sure you follow lazy inititializaion
+    console.log('Its an expensive process', Date.now())
+    return 0;
   }
+
+  const [i, setCount] = useState(() => initialState());
+  // const decreaseValue = () => {
+  //   setCount(i - 1);
+  // }
+  const changeCounter = (op) => {
+    if (op === '+') {
+      setCount(i + 1);
+    } else {
+      setCount(i - 1);
+    }
+
+  }
+
   return (
     <div className="App">
       <h1>{i}</h1>
-      <button onClick={addValue}>Click</button>
+      {/* ON Click is an event */}
+      {/* <button onClick={() => setCount(i + 1)}>Click</button> */}
+      {/* Arrow function or return must be gurantted when we pass parameter to fn on Click Events */}
+      <button onClick={() => changeCounter('+')}>Decrement</button>
+      <button onClick={() => changeCounter('-')}>Increment</button>
     </div>
   );
 }
