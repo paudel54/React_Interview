86ad1c588d151bdda53361a08bed274cc7654f7c
paudel54 <2sansrit@gmail.com>
2023-06-08 05:30:33 +0545

    Accomplished challange task for multiple state management and single handler for state updates

--- second/src/App.js
+++ second/src/App.js
@@ -1,53 +1,38 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  // Converting multiple state as Object. 
-  const [data, setData] = useState("Data for UPdates");
-  // const [name, setName] = useState("")
-  // const [address, setAddress] = useState("");
-  // const [phone, setPhone] = useState("");
+  const [data, setData] = useState("Loading Default...")
+
+  // managinf multiple state on Object:
 
   const [personInfo, setPersonInfo] = useState({
     name: '',
     address: '',
     phone: ''
-  });
+  })
 
-  const sendData = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+    console.log('FOrm Response Got Submitted. ')
     setData(`Updated Name is ${personInfo.name} your New Address is  ${personInfo.address} and Updated Phone Number is ${personInfo.phone}`);
-    console.log("Fom Submitted");
+
   }
-  // updating individual value of state inside an object by the use of Spread operator
-  // Creating dynamic handlers single change handler 
-  // const updateName = (e) => {
-  //   setPersonInfo({ ...personInfo, name: e.target.value });
-  // }
-
-  // const updateAddress = (e) => {
-  //   setPersonInfo({ ...personInfo, address: e.target.value });
-  // }
-
-  // const updateNumber = (e) => {
-  //   setPersonInfo({ ...personInfo, phone: e.target.value });
-  // }
-
-  const inputChangeHandler = (e) => {
-    // in ES6 adding of  square bracket makes it as variable [] 
-    // Creating a dynamicKey for Input so we can point the name of particular element for feild updates. 
+  const handleChange = (e) => {
     setPersonInfo({ ...personInfo, [e.target.name]: e.target.value })
   }
 
   return (
     <div>
-      <form onSubmit={sendData}>
-        <input onChange={inputChangeHandler} type='text' placeholder='Your Name' name="name" />
-        <input onChange={inputChangeHandler} type='text' placeholder='Address' name="address" />
-        <input onChange={inputChangeHandler} type='text' placeholder='Phone Number' name="phone" />
-        <button >Submit</button>
-        <h1>{JSON.stringify(personInfo)}</h1>
-        <h1>{data}</h1>
-      </form>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input onChange={handleChange} type="text" placeholder='Name' name="name" />
+          <input onChange={handleChange} type="text" placeholder='Address' name="address" />
+          <input onChange={handleChange} type="text" placeholder='Phone' name="phone" />
+          <button>Submit</button>
+          <h1>{data}</h1>
+          <h2>{JSON.stringify(personInfo)}</h2>
+        </form>
+      </div>
     </div>
   )
 }
---
+++ second/src/component/test/temp.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    // Converting multiple state as Object. 
+    const [data, setData] = useState("Data for UPdates");
+    // const [name, setName] = useState("")
+    // const [address, setAddress] = useState("");
+    // const [phone, setPhone] = useState("");
+
+    const [personInfo, setPersonInfo] = useState({
+        name: '',
+        address: '',
+        phone: ''
+    });
+
+    const sendData = (e) => {
+        e.preventDefault();
+        setData(`Updated Name is ${personInfo.name} your New Address is  ${personInfo.address} and Updated Phone Number is ${personInfo.phone}`);
+        console.log("Fom Submitted");
+    }
+    // updating individual value of state inside an object by the use of Spread operator
+    // Creating dynamic handlers single change handler 
+    // const updateName = (e) => {
+    //   setPersonInfo({ ...personInfo, name: e.target.value });
+    // }
+
+    // const updateAddress = (e) => {
+    //   setPersonInfo({ ...personInfo, address: e.target.value });
+    // }
+
+    // const updateNumber = (e) => {
+    //   setPersonInfo({ ...personInfo, phone: e.target.value });
+    // }
+
+    const inputChangeHandler = (e) => {
+        // in ES6 adding of  square bracket makes it as variable [] 
+        // Creating a dynamicKey for Input so we can point the name of particular element for feild updates. 
+        setPersonInfo({ ...personInfo, [e.target.name]: e.target.value })
+    }
+
+    return (
+        <div>
+            <form onSubmit={sendData}>
+                <input onChange={inputChangeHandler} type='text' placeholder='Your Name' name="name" />
+                <input onChange={inputChangeHandler} type='text' placeholder='Address' name="address" />
+                <input onChange={inputChangeHandler} type='text' placeholder='Phone Number' name="phone" />
+                <button >Submit</button>
+                <h1>{JSON.stringify(personInfo)}</h1>
+                <h1>{data}</h1>
+            </form>
+        </div>
+    )
+}
+
+export default App
