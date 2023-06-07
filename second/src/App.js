b2acd12dddafca61ea2e3d011703dc80fa4190f4
paudel54 <2sansrit@gmail.com>
2023-06-07 12:40:39 +0545

    Implemented state updates as object, Clearified

--- second/src/App.js
+++ second/src/App.js
@@ -1,31 +1,44 @@
 import React, { useState } from 'react'
 
 const App = () => {
+  // Converting multiple state as Object. 
   const [data, setData] = useState("Data for UPdates");
+  const [name, setName] = useState("")
   const [address, setAddress] = useState("");
   const [phone, setPhone] = useState("");
 
+  const [personInfo, setPersonInfo] = useState({
+    name: '',
+    address: '',
+    phone: ''
+  });
+
   const sendData = (e) => {
     e.preventDefault();
-    setData(`your New Address is  ${address} and Updated Phone Number is ${phone}`);
+    setData(`Updated Name is ${personInfo.name} your New Address is  ${personInfo.address} and Updated Phone Number is ${personInfo.phone}`);
     console.log("Fom Submitted");
   }
 
+  const updateName = (e) => {
+    setPersonInfo({ ...personInfo, name: e.target.value });
+  }
+
   const updateAddress = (e) => {
-    setAddress(e.target.value);
+    setPersonInfo({ ...personInfo, address: e.target.value });
   }
 
   const updateNumber = (e) => {
-    setPhone(e.target.value);
+    setPersonInfo({ ...personInfo, phone: e.target.value });
   }
-
 
   return (
     <div>
       <form onSubmit={sendData}>
+        <input onChange={updateName} type='text' placeholder='Your Name' />
         <input onChange={updateAddress} type='text' placeholder='Address' />
         <input onChange={updateNumber} type='text' placeholder='Phone Number' />
         <button >Submit</button>
+        <h1>{JSON.stringify(personInfo)}</h1>
         <h1>{data}</h1>
       </form>
     </div>
