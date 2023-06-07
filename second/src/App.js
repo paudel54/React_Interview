4afc1d12537132fbbe39f77248934a04d7f00ae9
paudel54 <2sansrit@gmail.com>
2023-06-07 09:52:15 +0545

    Clerified function return on events with parameter in React

--- second/src/App.js
+++ second/src/App.js
@@ -5,9 +5,23 @@ const App = () => {
     pCode: 1008,
     pName: 'Samsung'
   }
+
+  const getData = () => {
+    console.log('Checking the onClick Event on Button.')
+  }
+
   return (
     <div>
-      <DemoComponent value={1 + 4} date={new Date()} lekh='String literals not required curly brackets' test {...product} />
+      <DemoComponent value={1 + 4} date={new Date()} lekh='String literals not required curly brackets' test {...product} >
+        HEllo Guys, I am content on COmponent, Some calls me Children!!
+      </DemoComponent>
+      <p>Welcome to Event Handling Working with textbox checkbox </p>
+      {/* On Click must be or event be CamelCase.  */}
+      {/* if we use parantheis that function will be called immediately on load */}
+      {/* on passing parameter or args we need to have a fuction with return so it prevents on load behaviour.  */}
+      <button onClick={() => getData()}>
+        Get Data
+      </button>
     </div>
   )
 }
