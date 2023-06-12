152a3f491c47dc6ebde83c99230c287c88140037
paudel54 <2sansrit@gmail.com>
2023-06-12 21:33:19 +0545

    revisited TypeScript variables and dataTypes

--- tsr/src/ts/first.js
+++ tsr/src/ts/first.js
@@ -3,3 +3,11 @@ var sum = function (a, b) {
 };
 var total = sum(3, 9);
 console.log('Total Sum of numbers is : ', total);
+// Variables and datatypes: 
+//TS
+// let a; //type any
+//in JS
+//let a; // undefined. 
+var a;
+a = "test";
+console.log(a);
--- tsr/src/ts/first.ts
+++ tsr/src/ts/first.ts
@@ -1,6 +1,15 @@
-const sum = (a,b)=>{
+const sum = (a:number,b:number)=>{
     return a+b;
 }
 
-let total = sum(3,9);
+let total = sum(8,9);
 console.log('Total Sum of numbers is : ', total); 
+
+// Variables and datatypes: 
+//TS
+// let a; //type any
+//in JS
+//let a; // undefined. 
+
+// let person:string = 'Paudel';
+
