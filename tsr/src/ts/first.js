20a75f1d55420141c2818a3b065683f51d6f8c51
paudel54 <2sansrit@gmail.com>
2023-06-12 20:56:39 +0545

    typeScript revisited playaround with tsc

---
+++ tsr/src/ts/first.js
@@ -0,0 +1,5 @@
+var sum = function (a, b) {
+    return a + b;
+};
+var total = sum(3, 9);
+console.log('Total Sum of numbers is : ', total);
---
+++ tsr/src/ts/first.ts
@@ -0,0 +1,6 @@
+const sum = (a,b)=>{
+    return a+b;
+}
+
+let total = sum(3,9);
+console.log('Total Sum of numbers is : ', total);
