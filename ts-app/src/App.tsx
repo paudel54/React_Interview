82f1dfdfb81dc2a8c9f8fb7b54c197546c3968af
paudel54 <2sansrit@gmail.com>
2023-06-13 13:07:44 +0545

    Ts with dropdown init

--- ts-app/src/App.tsx
+++ ts-app/src/App.tsx
@@ -6,9 +6,16 @@ function App() {
   //  | is a pipe that signifies value can be of number or String.
   // const [counter, setCounter] = useState<number | string>("Paradise");
   const [counter, setCounter] = useState<number>(1);
+  // state for inputes or onChnage events.
+  const [txt, setTxt] = useState<string>("");
 
   const btnHandler = (e: React.MouseEvent<HTMLButtonElement>): void => {
     setCounter(counter + 1);
+  };
+
+  // Typescript of input handler
+  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setTxt(e.target.value);
   };
 
   return (
@@ -18,6 +25,10 @@ function App() {
       <h1>{counter}</h1>
       {/* if button is modified to input then there would be type error that TypeScript handles.  */}
       <button onClick={btnHandler}>Increment</button>
+      <hr />
+      <p>{txt}</p>
+      <hr />
+      <input type="text" onChange={inputChangeHandler} />
     </div>
   );
 }
