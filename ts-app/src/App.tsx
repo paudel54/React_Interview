9580ca33642512f0ca73e710ab57718801af12f9
paudel54 <2sansrit@gmail.com>
2023-06-13 09:56:08 +0545

    useState revisited with TypeScript

--- ts-app/src/App.tsx
+++ ts-app/src/App.tsx
@@ -1,10 +1,15 @@
 import Product from "./components/Product";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  //  | is a pipe that signifies value can be of number or String.
+  const [counter, setCounter] = useState<number | string>("Paradise");
+
   return (
     <div className="App">
       <Product pCode={1} pName="Apple" qty={7} />
+      <h1>{counter}</h1>
     </div>
   );
 }
