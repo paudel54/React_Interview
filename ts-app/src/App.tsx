e09f21f6f67bbf2c27d84fb45a93954539ed66f9
paudel54 <2sansrit@gmail.com>
2023-06-13 13:24:01 +0545

    working with dropdown html select type

--- ts-app/src/App.tsx
+++ ts-app/src/App.tsx
@@ -8,14 +8,19 @@ function App() {
   const [counter, setCounter] = useState<number>(1);
   // state for inputes or onChnage events.
   const [txt, setTxt] = useState<string>("");
+  const [select, setSelect] = useState<string>("");
 
   const btnHandler = (e: React.MouseEvent<HTMLButtonElement>): void => {
     setCounter(counter + 1);
   };
 
-  // Typescript of input handler
+  // Typescript of input handler: it does not returns anything so it's return type is void:
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setTxt(e.target.value);
+  };
+
+  const selectHandler = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    setSelect(e.target.value);
   };
 
   return (
@@ -28,7 +33,17 @@ function App() {
       <hr />
       <p>{txt}</p>
       <hr />
+      <p>The State from the dropDown menus</p>
+      <h1> {select}</h1>
       <input type="text" onChange={inputChangeHandler} />
+      <div>
+        <select onChange={selectHandler}>
+          <option value="Apple">Apple</option>
+          <option value="Banana">Banana</option>
+          <option value="Grapes">Grapes</option>
+          <option value="Orange">Orange</option>
+        </select>
+      </div>
     </div>
   );
 }
